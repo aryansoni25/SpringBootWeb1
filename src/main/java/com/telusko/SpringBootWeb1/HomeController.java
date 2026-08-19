cb6535bf0460b74f0a41d2ac39f77dc99b9f4daa
aryansoni25 <ashoksoni9425@gmail.com>
2026-08-19 21:14:43 +0530

    Initial commit

---
+++ src/main/java/com/telusko/SpringBootWeb1/HomeController.java
@@ -0,0 +1,33 @@
+package com.telusko.SpringBootWeb1;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        System.out.println("Home method get called ");
+        return "index";
+    }
+
+    @RequestMapping("add")
+    public ModelAndView add(int num1, int num2, ModelAndView model){
+
+        int res=num1+num2;
+        model.addObject("result",res);
+        model.setViewName("result");
+        return model;
+    }
+
+    @RequestMapping("addAlien")
+    public String addAlien( Alien alien){
+        return "result";
+    }
+}
